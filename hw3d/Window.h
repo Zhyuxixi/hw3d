782ec451e1c1a629316415a4add77837fa95118c
Zhyuxixi <zyx3211977@163.com>
2022-12-02 18:57:47 +0800

    some basic error check and add chili icon

--- hw3d/Window.h
+++ hw3d/Window.h
@@ -30,7 +30,7 @@ public:
 		Exception(int line, const char* file, HRESULT hr) noexcept;
 		const char* what() const noexcept override;
 		virtual const char* GetType() const noexcept;
-		static std::string TranslateErrorCode(HRESULT hr);
+		static std::string TranslateErrorCode(HRESULT hr) noexcept;
 		HRESULT GetErrorCode() const noexcept;
 		std::string GetErrorString() const noexcept;
 	private:
@@ -53,7 +53,7 @@ private:
 		HINSTANCE hInst;
 	};
 public:
-	Window(int width, int height, const char* name) noexcept;
+	Window(int width, int height, const char* name);
 	~Window();
 	Window(const Window&) = delete;
 	Window& operator=(const Window&) = delete;
@@ -69,3 +69,4 @@ private:
 
 // error exception helper macro
 #define CHWND_EXCEPT(hr) Window::Exception(__LINE__, __FILE__, hr)
+#define CHWND_LAST_EXCEPT() Window::Exception( __LINE__,__FILE__,GetLastError() )
