30c2c6a4058efe19c629b5b5db3331678b63bcab
Zhyuxixi <zyx3211977@163.com>
2022-12-07 00:59:41 +0800

    geometry primitives

--- hw3d/App.cpp
+++ hw3d/App.cpp
@@ -1,32 +1,32 @@
 #include "App.h"
-#include "Box.h"
+#include "BaseDraw.h"
 #include <memory>
 
 App::App()
 	:
-	wnd( 800,600,"The Donkey Fart Box" )
+	wnd(800, 600, "The Donkey Fart Box")
 {
-	std::mt19937 rng( std::random_device{}() );
-	std::uniform_real_distribution<float> adist( 0.0f,3.1415f * 2.0f );
-	std::uniform_real_distribution<float> ddist( 0.0f,3.1415f * 2.0f );
-	std::uniform_real_distribution<float> odist( 0.0f,3.1415f * 0.3f );
-	std::uniform_real_distribution<float> rdist( 6.0f,20.0f );
-	for( auto i = 0; i < 80; i++ )
+	std::mt19937 rng(std::random_device{}());
+	std::uniform_real_distribution<float> adist(0.0f, 3.1415f * 2.0f);
+	std::uniform_real_distribution<float> ddist(0.0f, 3.1415f * 1.0f);
+	std::uniform_real_distribution<float> odist(0.0f, 3.1415f * 0.08f);
+	std::uniform_real_distribution<float> rdist(6.0f, 20.0f);
+	for (auto i = 0; i < 180; i++)
 	{
-		boxes.push_back( std::make_unique<Box>(
-			wnd.Gfx(),rng,adist,
-			ddist,odist,rdist
-		) );
+		boxes.push_back(std::make_unique<BaseDraw>(
+			wnd.Gfx(), rng, adist,
+			ddist, odist, rdist
+			));
 	}
-	wnd.Gfx().SetProjection( DirectX::XMMatrixPerspectiveLH( 1.0f,3.0f / 4.0f,0.5f,40.0f ) );
+	wnd.Gfx().SetProjection(DirectX::XMMatrixPerspectiveLH(1.0f, 3.0f / 4.0f, 0.5f, 40.0f));
 }
 
 int App::Go()
 {
-	while( true )
+	while (true)
 	{
 		// process all messages pending, but to not block for new messages
-		if( const auto ecode = Window::ProcessMessages() )
+		if (const auto ecode = Window::ProcessMessages())
 		{
 			// if return optional has value, means we're quitting so return exit code
 			return *ecode;
@@ -41,11 +41,11 @@ App::~App()
 void App::DoFrame()
 {
 	auto dt = timer.Mark();
-	wnd.Gfx().ClearBuffer( 0.07f,0.0f,0.12f );
-	for( auto& b : boxes )
+	wnd.Gfx().ClearBuffer(0.07f, 0.0f, 0.12f);
+	for (auto& b : boxes)
 	{
-		b->Update( dt );
-		b->Draw( wnd.Gfx() );
+		b->Update(dt);
+		b->Draw(wnd.Gfx());
 	}
 	wnd.Gfx().EndFrame();
 }
--- hw3d/DrawableBase.h
+++ hw3d/DrawableBase.h
@@ -3,23 +3,36 @@
 #include "IndexBuffer.h"
 
 template<class T>
-class DrawableBase : public Drawable
+class DrawableBase : public Drawable	//用来绘制同一种图形基元
 {
-public:
-	bool IsStaticInitialized() const noexcept
+protected:
+	static bool IsStaticInitialized() noexcept
 	{
 		return !staticBinds.empty();
 	}
-	void AddStaticBind(std::unique_ptr<Bindable> bind) noexcept(!IS_DEBUG)
+	static void AddStaticBind(std::unique_ptr<Bindable> bind) noexcept(!IS_DEBUG)
 	{
-		assert("*Must* use AddIndexBuffer to bind index buffer" && typeid(*bind) != typeid(IndexBuffer));
+		assert("*Must* use AddStaticIndexBuffer to bind index buffer" && typeid(*bind) != typeid(IndexBuffer));
 		staticBinds.push_back(std::move(bind));
 	}
 	void AddStaticIndexBuffer(std::unique_ptr<IndexBuffer> ibuf) noexcept(!IS_DEBUG)
 	{
-		assert(pIndexBuffer == nullptr);
+		assert("Attempting to add index buffer a second time" && pIndexBuffer == nullptr);
 		pIndexBuffer = ibuf.get();
 		staticBinds.push_back(std::move(ibuf));
+	}
+	void SetIndexFromStatic() noexcept(!IS_DEBUG)
+	{
+		assert("Attempting to add index buffer a second time" && pIndexBuffer == nullptr);
+		for (const auto& b : staticBinds)
+		{
+			if (const auto p = dynamic_cast<IndexBuffer*>(b.get()))
+			{
+				pIndexBuffer = p;
+				return;
+			}
+		}
+		assert("Failed to find index buffer in static binds" && pIndexBuffer != nullptr);
 	}
 private:
 	const std::vector<std::unique_ptr<Bindable>>& GetStaticBinds() const noexcept override
